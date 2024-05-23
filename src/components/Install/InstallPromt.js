ab3f198ae9f8e12ffc0685b9c4a951fab03aae01
heavyrain0706 <heavyrain0706@gmail.com>
2024-05-23 13:42:54 +0300

    hotfix

--- src/components/Install/InstallPromt.js
+++ src/components/Install/InstallPromt.js
@@ -10,27 +10,32 @@ const InstallPrompt = () => {
       e.preventDefault();
       if (!installPromptDismissed) {
         setDeferredPrompt(e);
-        setTimeout(() => {
-          e.prompt();
-          e.userChoice.then((choiceResult) => {
-            if (choiceResult.outcome === 'accepted') {
-              console.log('User accepted the install prompt');
-            } else {
-              console.log('User dismissed the install prompt');
-              localStorage.setItem('installPromptDismissed', 'true');
-            }
-            setDeferredPrompt(null);
-          });
-        }, 5000); 
       }
     };
 
     window.addEventListener('beforeinstallprompt', beforeInstallPromptHandler);
 
+    if (deferredPrompt && !installPromptDismissed) {
+      const timer = setTimeout(() => {
+        deferredPrompt.prompt();
+        deferredPrompt.userChoice.then((choiceResult) => {
+          if (choiceResult.outcome === 'accepted') {
+            console.log('User accepted the install prompt');
+          } else {
+            console.log('User dismissed the install prompt');
+            localStorage.setItem('installPromptDismissed', 'true');
+          }
+          setDeferredPrompt(null);
+        });
+      }, 5000); 
+
+      return () => clearTimeout(timer);
+    }
+
     return () => {
       window.removeEventListener('beforeinstallprompt', beforeInstallPromptHandler);
     };
-  }, []);
+  }, [deferredPrompt]);
 
   return null;
 };
