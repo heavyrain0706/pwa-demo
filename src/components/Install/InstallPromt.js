70f435e24671d409316f2bb18d0704e567980c1c
heavyrain0706 <heavyrain0706@gmail.com>
2024-05-23 13:20:19 +0300

    install on touch

--- src/components/Install/InstallPromt.js
+++ src/components/Install/InstallPromt.js
@@ -13,31 +13,37 @@ const InstallPrompt = () => {
       }
     };
 
-    const clickHandler = () => {
-      if (deferredPrompt && !installPromptDismissed) {
-        deferredPrompt.prompt();
-        deferredPrompt.userChoice.then((choiceResult) => {
-          if (choiceResult.outcome === 'accepted') {
-            console.log('User accepted the install prompt');
-          } else {
-            console.log('User dismissed the install prompt');
-            localStorage.setItem('installPromptDismissed', 'true');
-          }
-          setDeferredPrompt(null);
-        });
-      }
-    };
-
     window.addEventListener('beforeinstallprompt', beforeInstallPromptHandler);
-    document.addEventListener('click', clickHandler);
 
     return () => {
       window.removeEventListener('beforeinstallprompt', beforeInstallPromptHandler);
-      document.removeEventListener('click', clickHandler);
     };
-  }, [deferredPrompt]);
+  }, []);
 
-  return null;
+  const handleInstallTouch = () => {
+    if (deferredPrompt) {
+      deferredPrompt.prompt();
+      deferredPrompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted the install prompt');
+        } else {
+          console.log('User dismissed the install prompt');
+          localStorage.setItem('installPromptDismissed', 'true');
+        }
+        setDeferredPrompt(null);
+      });
+    }
+  };
+
+  return (
+    <div>
+      {deferredPrompt && (
+        <button onTouchEnd={handleInstallTouch}>
+          Установить приложение
+        </button>
+      )}
+    </div>
+  );
 };
 
 export default InstallPrompt;
